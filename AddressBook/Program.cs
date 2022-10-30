43dedf2f90b3cd562a58de8c7bc1ec5de5e798d0
AkashNarale <akashnarale43@gmail.com>
2022-10-30 13:26:36 +0530

    Uc1

---
+++ AddressBook/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome to your Address Book--");
+
+            //creating object person1 of AddressBook Class()
+            AddressBook person1 = new AddressBook();
+            while (true)
+            {
+                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-Exit from the Addressbook");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        person1.AddPerson();
+                        Console.WriteLine("Enter any key to exit");
+                        break;
+                    case 2:
+                        return;
+                    default:
+                        Console.WriteLine("Please enter a valid input");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
