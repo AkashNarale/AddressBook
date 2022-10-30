15f05baedd131a88ffefc39eee499ac04cf4f79a
AkashNarale <akashnarale43@gmail.com>
2022-10-30 14:17:04 +0530

    UC3

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -18,7 +18,7 @@ namespace AddressBook
             AddressBook person1 = new AddressBook();
             while (true)
             {
-                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook \n 3-Exit from the Addressbook");
+                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook \n 3-Edit the Address Book \n 4-Exit from the Addressbook");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -31,6 +31,10 @@ namespace AddressBook
                         Console.WriteLine("Enter any key to exit");
                         break;
                     case 3:
+                        person1.Edit();
+                        Console.WriteLine("Enter any key to exit");
+                        break;
+                    case 4:
                         return;
                     default:
                         Console.WriteLine("Please enter a valid input");
